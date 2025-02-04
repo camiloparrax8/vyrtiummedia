2a4c3c1f788500b183ebd1f7c85a98b05bfb5cfc
PC-HP <cparraurueta@gmail.com>
2025-02-04 18:43:46 -0500

    styles(HomeBanner): animate button in image

--- src/app/components/home_banner.js
+++ src/app/components/home_banner.js
@@ -8,7 +8,6 @@ export default function HomeBanner() {
                 {/* home banner content */}
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4 pt-8 px-8 md:px-20 h-full">
                     <div className="flex flex-col gap-6 w-full md:pb-16 pt-4 max-w-[550px]">
-                       
                         <div className="relative border-y-[2px] border-primary px-4 py-2 flex items-center justify-center">
                                 <div className="absolute left-0 top-0 h-1/2 w-[2px] bg-primary">
                                     <div className="absolute bottom-0 left-0 w-2.5 h-2.5 bg-primary rounded-full translate-x-[-50%]" />
@@ -23,7 +22,11 @@ export default function HomeBanner() {
                     </div>
                     <div className="relative place-self-center md:place-self-end">
                         <Image src="/images/person-banner.png" alt="Banner" width={390} height={390} />
-                        <Image src="/icons/button-banner.png" alt="Banner" width={80} height={80} className="absolute top-[46%] left-[15%]"/>
+                        <div className="absolute z-10 top-[50%] left-[22%]">
+                            <Image src="/icons/button-banner2.png" alt="Banner" width={40} height={40} className="animate-pulse"/>
+                            <Image src="/icons/button-banner.png" alt="Banner" width={18} height={18} className="absolute top-[30%] left-[25%]"/>
+                        </div>
+                        
                     </div>  
                 </div>
             </div>
