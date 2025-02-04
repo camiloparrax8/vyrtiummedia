5db25509347583f9188c0f8591bf62616514b3fb
PC-HP <cparraurueta@gmail.com>
2025-02-04 11:13:47 -0500

    style(SectionTitle): change text align

--- src/app/components/section-title.js
+++ src/app/components/section-title.js
@@ -11,8 +11,8 @@ export default function SectionTitle({ title, highlight, icon_url }) {
         <div className="w-full">
             <div className="flex flex-row gap-2 md:gap-4 items-center justify-between mb-10">
                 <div className="flex gap-4 flex-row items-center text-center">
-                    <Image src={icon_url} alt="icon profit" width={60} height={63} className="md:w-[90px] md:h-[95px]" />
-                    <Title title={title} highlight={highlight} className="font-bold text-[24px] md:text-[52px] leading-tight text-left md:text-center" />
+                    <Image src={icon_url} alt="icon" width={60} height={63} className="md:w-[90px] md:h-[95px]" />
+                    <Title title={title} highlight={highlight} className="font-bold text-[24px] md:text-[52px] leading-tight text-left md:text-left" />
                 </div>
                 <div className="flex items-center justify-start w-[20%] md:w-[60%] mt-4 md:mt-0 flex-nowrap">
                     <div className="w-2.5 h-2.5 bg-primary rounded-full shrink-0"></div>
